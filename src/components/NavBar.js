9191834c52fe9c0aaebf60edd187cb414e67fe71
Tyrone <tyrone.cloma0219@gmail.com>
2022-07-21 10:57:26 -0400

    commit tree 79a1437a7ac6b3c74703ae424759032d7744ea0e

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,13 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{
+    links.map((element) => {
+      return(
+        <a href={'#'+element} key={element}>{element}</a>
+      )
+    })
+    }</nav>;
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -6,7 +6,11 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {
+          technologies.map((technologies) => {
+            return <span>{technologies}</span>
+          })
+        }
       </div>
     </div>
   );
